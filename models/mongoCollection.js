2b6e9d510c95a6eeb11938a55b02e4376a48386d
bj <lpaben62@gmail.com>
2017-05-12 15:02:19 +0800

    fix mongoCollection.js

--- models/mongoCollection.js
+++ models/mongoCollection.js
@@ -2,18 +2,18 @@ var connection = require('./mongoConnection.js');
 
 let dbConnectPromise;
 
-function getCollection(collectionName){
-    if(!dbConnectPromise){
+function getCollection(collectionName) {
+    if (!dbConnectPromise) {
         dbConnectPromise = connection.connect();
-
-        return dbConnectPromise.then((db)=>{
-            return db.collection(collectionName);
-        }).catch((err)=>{
-            console.log(err);
-            return err;
-            // res.render('error', {error:err});
-        });
     }
+
+    return dbConnectPromise.then((db) => {
+        return db.collection(collectionName);
+    }).catch((err) => {
+        console.log(err);
+        return err;
+        // res.render('error', {error:err});
+    });
 }
 
 exports.getUserCollection = getCollection('users');
